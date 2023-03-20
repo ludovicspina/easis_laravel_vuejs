44af981badf0e21e57f3e3fbe4113391a6073f5d
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-20 03:55:28 +0100

    test cors

--- laravel_easis/app/Http/Middleware/CorsMiddleware.php
+++ laravel_easis/app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'localhost:5173', 'etheron.fr');
+        $response->headers->set('Access-Control-Allow-Origin', 'localhost:5173', 'etheron.fr', '82.66.222.253', '192.168.1.92');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', true);
