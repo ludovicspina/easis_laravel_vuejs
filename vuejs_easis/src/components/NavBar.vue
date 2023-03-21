5a73c3e4223711ee9c04446306a5bb769d45d70e
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-21 13:48:43 +0100

    update port

--- vuejs_easis/src/components/NavBar.vue
+++ vuejs_easis/src/components/NavBar.vue
@@ -71,7 +71,7 @@ export default {
       }
     },
     getUserRole(email) {
-      axios.get("http://api.etheron.fr/api/users")
+      axios.get("http://localhost:8000/api/users")
           .then((response) => {
             const tempRole = Object.assign([], response.data);
             tempRole.forEach(elem => {
@@ -87,7 +87,7 @@ export default {
     //user login function and api call
     login_user() {
       axios
-          .post('http://api.etheron.fr/api/login', this.user)
+          .post('http://localhost:8000/api/login', this.user)
           .then((resp) => {
             this.getUserRole(this.user.email);
             this.user.email = '';
