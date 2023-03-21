02c3fc6bc093bc8a8162cb81cae47c501e49aff4
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-21 01:28:38 +0100

    cors

--- laravel_easis/config/cors.php
+++ laravel_easis/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://etheron.fr', 'http://192.168.1.92:5173'],
+    'allowed_origins' => ['http://192.168.1.92:5173'],
 
     'allowed_origins_patterns' => [],
 
