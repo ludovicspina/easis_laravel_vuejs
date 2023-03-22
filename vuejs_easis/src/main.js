27a9fe08cbf9bf969bde1a68d9e7a7a4a856afc4
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-22 04:03:43 +0100

    a

--- vuejs_easis/src/main.js
+++ vuejs_easis/src/main.js
@@ -6,18 +6,18 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    )
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS, PATCH"
-    )
-    next()
-})
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*")
+//     res.setHeader(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//     )
+//     res.setHeader(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+//     )
+//     next()
+// })
 
 app.use(router)
 
