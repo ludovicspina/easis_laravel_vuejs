70d525eebdf2ccea9ef98f5ab237ce16b2e2bb08
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-22 04:30:39 +0100

    a

--- laravel_easis/app/Providers/RouteServiceProvider.php
+++ laravel_easis/app/Providers/RouteServiceProvider.php
@@ -36,6 +36,20 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 
+    function mapApiRoutes(): void
+    {
+        Route::group([
+            'middleware' => ['api', 'cors'],
+            'namespace' => $this->namespace,
+            'prefix' => 'api',
+        ], function ($router) {
+            //Add you routes here, for example:
+            Route::apiResource('/posts','PostController');
+        });
+    }
+
+
+
     /**
      * Configure the rate limiters for the application.
      */
