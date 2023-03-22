35216133825c5fe5ae4b3107008d4e2186b73c16
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-22 04:12:17 +0100

    a

--- laravel_easis/app/Http/Middleware/VerifyCsrfToken.php
+++ laravel_easis/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        ' http://192.168.1.92:5173/*',
+        'http://etheron.fr',
     ];
 }
