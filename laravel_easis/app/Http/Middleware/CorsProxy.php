11cefc9058c06cd2685f78d36963159246657b85
Ludovic SPINA <ludovic.spina@proton.me>
2023-03-22 13:55:22 +0100

    a

--- laravel_easis/app/Http/Middleware/CorsProxy.php
+++ laravel_easis/app/Http/Middleware/CorsProxy.php
@@ -11,7 +11,7 @@ class CorsProxy
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'http://etheron.fr');
+        $response->header('Access-Control-Allow-Origin', 'http://etheron.fr/*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
         $response->header('Access-Control-Allow-Credentials', 'true');
