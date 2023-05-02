3bb3c9b15b79ffdc20d277c5e1eb1cfeafbe8468
Ludovic SPINA <ludovic.spina@proton.me>
2023-05-02 16:31:20 +0200

    a

--- vuejs_easis/src/views/HomeView.vue
+++ vuejs_easis/src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="flex justify-center items-center flex-col">
-    <div class="text-4xl mb-10 bg-black bg-opacity-90 text-amber-300 p-4 rounded-xl border-2 border-amber-300">Site de la guilde Easis</div>
+    <div class="text-4xl mb-10 bg-black bg-opacity-75 text-amber-300 p-4 rounded-xl border-2 border-amber-300">Site de la guilde Easis</div>
   </div>
+
+  <div>Nombre d'instance réalisées : {{ nombreInstances }}</div>
 </template>
 <script>
 
 
-import RatiosView from "@/views/RatiosView.vue";
+import axios from "axios";
 
 export default {
-  components: {RatiosView},
+  data() {
+    return {
+      instances: [],
+      nombreInstances: 0,
+    };
+  },
   beforeMount() {
     this.getLoginStatus()
   },
@@ -17,6 +24,16 @@ export default {
     getLoginStatus() {
       this.isLoggedIn = localStorage.getItem('isLoggedIn');
     },
+    mounted() {
+      axios.get("http://api.etheron.fr/api/instances")
+          .then((response) => {
+            const tempsInstances = Object.assign([], response.data);
+            tempsInstances.forEach(elem => {
+              this.instances.push(elem);
+            })
+            this.nombreInstances = response.data.length;
+          })
+    }
   }
 }
 </script>
--- vuejs_easis/src/views/RatiosView.vue
+++ vuejs_easis/src/views/RatiosView.vue
@@ -104,28 +104,29 @@
               class="mb-1 grid grid-cols-5 hover:scale-125 cursor-pointer py-0.5 hover:bg-black hover:border hover:rounded hover:border-amber-300 hover:shadow hover:rounded transition delay-50">
             <div class="flex justify-center">
               <div class="flex justify-center items-center gap-1 overflow-x-hidden">
-
-                <img :src="joueur.icon" :alt="joueur.icon" class="h-6">
+                <div class="flex gap-1" v-if="joueur.min_carte !== null">{{ joueur.min_carte.toUpperCase() }}
+                  <img :src="joueur.icon" :alt="joueur.icon" class="h-6"></div>
+                <div v-else><img :src="joueur.icon" :alt="joueur.icon" class="h-6"></div>
                 <template v-if="joueur.familier === 's'">
-                  <img src="../../public/peteggs.png" class="h-6">
+                  <img src="../../peteggs.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'a'">
-                  <img src="../../public/petegga.png" class="h-6">
+                  <img src="../../petegga.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'b'">
-                  <img src="../../public/peteggb.png" class="h-6">
+                  <img src="../../peteggb.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'c'">
-                  <img src="../../public/peteggc.png" class="h-6">
+                  <img src="../../peteggc.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'd'">
-                  <img src="../../public/peteggd.png" class="h-6">
+                  <img src="../../peteggd.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'e'">
-                  <img src="../../public/petegge.png" class="h-6">
+                  <img src="../../petegge.png" class="h-6">
                 </template>
                 <template v-if="joueur.familier === 'f'">
-                  <img src="../../public/peteggf.png" class="h-6">
+                  <img src="../../peteggf.png" class="h-6">
                 </template>
                 {{ joueur.pseudo }}
 
